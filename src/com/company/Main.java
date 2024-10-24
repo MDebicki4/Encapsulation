af94031eb38a0660a68adffa2111eb63cef6cb19
Michal Debicki <michaldebicki4@gmail.com>
2024-10-24 20:12:28 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EnhancedPlayer player = new EnhancedPlayer("michas", 200, "Sword");
+        System.out.println(player.getHitPoints());
+    }
+}
